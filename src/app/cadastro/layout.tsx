7f0d642505b7cfa5bda527d0dcd113ae5a7ba516
dinbruno <brunodinosantos@outlook.com>
2024-05-25 14:48:06 -0300

    fix

--- src/app/cadastro/layout.tsx
+++ src/app/cadastro/layout.tsx
@@ -11,9 +11,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className="h-full w-full ">
       <body className="h-full bg-white">{children}</body>
