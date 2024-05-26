6c0c300306de1de1770eb60676ff4e8358285445
dinbruno <brunodinosantos@outlook.com>
2024-05-26 12:11:36 -0300

    fix

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,31 +1,10 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
-import { registerUser } from '../../services/firebaseService';
 import "../../app/globals.css";
+import RegisterComponent from "./@components/RegisterComponent";
 
-const SignupPage: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [familyName, setFamilyName] = useState('');
+function SignUpPage() {
+  return <RegisterComponent />;
+}
 
-  const handleSignup = async () => {
-    try {
-      const user = await registerUser(email, password, familyName);
-      console.log('Usuário registrado com sucesso:', user);
-    } catch (error) {
-      console.error('Erro no registro:', error);
-    }
-  };
-
-  return (
-    <div>
-      <input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} />
-      <input type="text" placeholder="Nome da Família (opcional)" onChange={(e) => setFamilyName(e.target.value)} />
-      <button onClick={handleSignup}>Registrar</button>
-    </div>
-  );
-};
-
-export default SignupPage;
+export default SignUpPage;
